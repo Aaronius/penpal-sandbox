40b0701bbd9cce9ce084ff3e1d414331a8cf09c9
Aaron Hardy <aaron@aaronhardy.com>
2020-08-05 20:44:03 -0600

    Initial commit.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// import { ErrorCode } from 'penpal';
 import logo from './logo.svg';
 import './App.css';
 
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      {/*{ ErrorCode.ConnectionDestroyed }*/}
     </div>
   );
 }
